14955cbfaeff03b82816745f536a19fb8b5312bf
HellotrLab <phuongtrangvu05@gmail.com>
2026-07-16 14:22:43 -0700

    add in personal spreadsheetid

--- rondo.js
+++ rondo.js
@@ -1,6 +1,6 @@
 /* Load site/pages/items from Google Sheets (gviz) and render site.
    Items list is independent of pages: no auto-filtering by page. */
-var spreadsheetID = "1iwIx8sqiBTqbtAI7TX4vDGDVee3dPz0wa3MazBv2fzQ";
+var spreadsheetID = "16tMQhJgJbdWd3xVvydGTRPC3t8xoHjuIdbj6ofj9I3Q";
 var siteSheet  = "1569296108";
 var pagesSheet = "28080804";
 var itemsSheet = "0";
